05cdc0a688e4a8c366dbdba52a1eda7aa8a93c6f
Raxit Zora <raxitzora@gmail.com>
2026-03-18 23:15:32 +0530

    agent programming started

--- agent.js
+++ agent.js
@@ -6,6 +6,8 @@ const client = new OpenAI({
   baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/",
 });
 
+
+
 // tools
 function executeCommand(cmd = "") {
   const result = execSync(cmd);
